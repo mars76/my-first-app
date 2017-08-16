11e0f0964c5504fb1c60df78b6ab7fe61337fb19
sateesh <kommineni.sateesh@gmail.com>
2017-08-15 21:39:20 -0400

    updating the version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {SampleModule} from 'my-angular-library';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SampleModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
